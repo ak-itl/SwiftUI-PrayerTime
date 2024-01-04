0dfc9152ff4012f5432dcc61dcd395cba38d9146
ak-itl <alainkeita21@gmail.com>
2024-01-04 17:13:36 -0500

    Initial Commit

---
+++ SwiftUI-PrayerTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-PrayerTime
+//
+//  Created by Alain Keita on 1/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-PrayerTime/SwiftUI_PrayerTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PrayerTimeApp.swift
+//  SwiftUI-PrayerTime
+//
+//  Created by Alain Keita on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PrayerTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
